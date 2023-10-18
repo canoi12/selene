578c491404d52f3bbbec1ace66c04e8a6dc74e23
Canoi <canoimaster@gmail.com>
2023-10-18 19:10:25 -0300

    filesystem read and write functions;

--- src/modules/fs/fs.c
+++ src/modules/fs/fs.c
@@ -18,6 +18,52 @@ static MODULE_FUNCTION(fs, Exists) {
     return 1;
 }
 
+static MODULE_FUNCTION(fs, Read) {
+    INIT_ARG();
+    CHECK_STRING(path);
+    #if defined(OS_WIN)
+        FILE* fp;
+        fopen_s(&fp, path, "rb");
+    #else
+        FILE* fp = fopen(path, "rb");
+    #endif
+
+    if (!fp)
+        return luaL_error(L, "Failed to read file: %s\n", path);
+
+    fseek(fp, 0, SEEK_END);
+    size_t size = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
+    char* data = malloc(size);
+    fread(data, 1, size, fp);
+    fclose(fp);
+
+    NEW_UDATA(Data, d);
+    d->data = data;
+    d->size = size;
+    return 1;
+}
+
+static MODULE_FUNCTION(fs, Write) {
+    INIT_ARG();
+    int len;
+    CHECK_STRING(path);
+    CHECK_LSTRING(text, &len);
+
+    #if defined(OS_WIN)
+        FILE* fp;
+        fopen_s(&fp, path, "w");
+    #else
+	    FILE* fp = fopen(path, "w");
+    #endif
+	if (!fp)
+        return luaL_error(L, "failed to create file %s\n", path);
+	fprintf(fp, "%*s", len, text);
+	fclose(fp);
+    PUSH_BOOLEAN(1);
+    return 1;
+}
+
 static MODULE_FUNCTION(fs, CreateDir) {
     INIT_ARG();
     CHECK_STRING(path);
@@ -46,6 +92,8 @@ static MODULE_FUNCTION(fs, Load) {
 BEGIN_MODULE(fs) {
     BEGIN_REG()
         REG_FIELD(fs, Exists),
+        REG_FIELD(fs, Read),
+        REG_FIELD(fs, Write),
         REG_FIELD(fs, CreateDir),
         REG_FIELD(fs, RemoveDir),
         REG_FIELD(fs, Load),
