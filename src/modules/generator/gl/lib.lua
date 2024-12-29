dc874876033b3459e4d8c31a1dc76fc704016a6d
Canoi <canoimaster@gmail.com>
2024-12-29 20:30:29 -0300

    updated emscripten macro to GLES2 in gl module;

--- src/modules/generator/gl/lib.lua
+++ src/modules/generator/gl/lib.lua
@@ -6,7 +6,7 @@ sources['Core'] = {
     {
         cdef =
 [[
-#ifndef OS_EMSCRIPTEN
+#ifndef GL_ES_VERSION_2_0
     INIT_ARG();
     if (lua_type(L, arg) == LUA_TLIGHTUSERDATA) {
         gladLoadGLLoader(lua_touserdata(L, arg));
