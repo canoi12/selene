289e1841d16c383f337c1e509fd64bdeaf83d4fd
Canoi <canoimaster@gmail.com>
2024-12-29 11:50:36 -0300

    added annotations for json and image modules

---
+++ annotations/image.lua
@@ -0,0 +1,12 @@
+--- @meta
+
+image = {}
+
+--- @param filename string
+--- @return table
+function image.from_file(filename) end
+
+--- @param data selene.Data
+--- @param size integer
+--- @return table
+function image.from_memory(data, size) end
---
+++ annotations/json.lua
@@ -0,0 +1,10 @@
+--- @meta
+json = {}
+
+--- @param str string
+--- @return table
+function json.decode(str) end
+
+--- @param t table
+--- @return string
+function json.encode(t) end
