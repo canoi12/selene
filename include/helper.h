a0558ddc3ac31d3d5304c6b76766b2853aa560f4
Canoi <canoimaster@gmail.com>
2025-03-17 07:20:32 -0300

    updated function utf8_codepoint to returns only the bytes to move;

--- include/helper.h
+++ include/helper.h
@@ -44,36 +44,40 @@ void char_rect(FontGlyph* glyphs, const int c, float *x, float *y, int* out_pos,
 }
 
 
-uint8_t* utf8_codepoint(const uint8_t* p, int* codepoint) {
-    uint8_t* n = NULL;
+int utf8_codepoint(uint8_t* p, int* codepoint) {
+    //uint8_t* n = NULL;
+    int n = 0;
     uint8_t c = *p;
     if (c < 0x80) { // Se for um ascii normal, o codepoint é o próprio caractere
         *codepoint = c;
-        n = (uint8_t*)p + 1;
-        return n;
+        return 1;
     }
 
     switch (c & 0xf0) { // Checa o primeiro byte para conferir quantos bytes precisam ser usados
     case 0xf0: { // 4 bytes
         *codepoint = ((p[0] & 0x07) << 18) | ((p[1] & 0x3f) << 12) | ((p[2] & 0x3f) << 6) | ((p[3] & 0x3f));
-        n = (uint8_t*)p + 4;
+        //n = (uint8_t*)p + 4;
+        n = 4;
         break;
     }
     case 0xe0: { // 3 bytes
         *codepoint = ((p[0] & 0x0f) << 12) | ((p[1] & 0x3f) << 6) | ((p[2] & 0x3f));
-        n = (uint8_t*)p + 3;
+        //n = (uint8_t*)p + 3;
+        n = 3;
         break;
     }
     case 0xc0:
     case 0xd0: { // 2 bytes
         *codepoint = ((p[0] & 0x1f) << 6) | ((p[1] & 0x3f));
-        n = (uint8_t*)p + 2;
+        //n = (uint8_t*)p + 2;
+        n = 2;
         break;
     }
     default:
     {
         *codepoint = -1;
-        n = n + 1;
+        n = 1;
+        //n = n + 1;
     }
     }
     if (*codepoint > SELENE_MAX_UNICODE) *codepoint = -1;
