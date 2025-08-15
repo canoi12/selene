2e48eee2e11099af10bb14fffe729a977674dfd6
canoi <eu@canoigomes.com.br>
2025-08-15 07:41:29 -0300

    fixed module renderer wrong path

--- src/modules/renderer/module.c
+++ src/modules/renderer/module.c
@@ -15,14 +15,19 @@ static int l_renderer__destroy(lua_State* L) {
 
 int luaopen_renderer(lua_State* L) {
     const luaL_Reg _reg[] = {
-        {"create", NULL},
+        {"create", l_renderer_create},
         {NULL, NULL}
     };
+    luaL_newlib(L, _reg);
 
     luaL_newmetatable(L, "selene.Renderer");
+    lua_pushvalue(L, -1);
+    lua_setfield(L, -2, "__index");
     const luaL_Reg _meta[] = {
-        {"destroy", NULL},
+        {"destroy", l_renderer__destroy},
         {NULL, NULL}
     };
+    luaL_setfuncs(L, _meta, 0);
+    lua_setfield(L, -2, "Renderer");
     return 1;
 }
